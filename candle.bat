370de8f4785a3d04d331e6ad439dd5bded0534bd
shanedlee <slee@saltstack.com>
2015-02-28 00:58:54 +0000

    Added custom dialog... not compiling

--- candle.bat
+++ candle.bat
@@ -1,4 +1,6 @@
 set source1=.\salt.wxs
 set source2=.\salt_files.wxs
+set source3=.\SaltUI_Mondo.wxs
+set source4=.\SaltExitDialog.wxs
 set output=-out .\
-"%wix%bin\candle.exe" %source1% %source2% %output%
+candle.exe %source1% %source2% %source3% %source4% %output%
--- light.bat
+++ light.bat
@@ -1,6 +1,7 @@
-set source=.\
-set source1=%source%salt.wixobj
-set source2=%source%salt_files.wixobj
+set source1=salt.wixobj
+set source2=salt_files.wixobj
+set source3=SaltUI_Mondo.wixobj
+set source4=SaltExitDialog.wixobj
 set output=%source%salt.msi
 
 light.exe %source1% %source2% -o %output% -ext WixUIExtension
